7b1160bc6f85313253026fb7f14dbc6b71ddaf87
Ostap Blystsiv <ostap.blystsiv@gmail.com>
2023-07-03 13:41:43 +0300

    feat(server): Add ESLint and Prettier configuration

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-dotenv.config();
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
 
-const PORT = process.env.PORT || 5050;
+dotenv.config()
+const app = express()
 
-const app = express();
+const PORT = process.env.PORT || 5050
 
-app.use(cors());
+app.use(cors())
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(PORT, () => {
-	console.log(`Server running on  http://localhost:${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server running on http://localhost:${PORT}`)
 })
